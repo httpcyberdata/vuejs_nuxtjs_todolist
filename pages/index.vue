8a0b35eb75d7413a16e81578967d1ff85d253ec5
httpcyberdata <borgeringstad@Brge-Ringstad-PC.local>
2022-11-15 23:37:31 -0500

    added input field w/ keypress.enter

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    Hello, world.
+  <main>
+    <h1>Task Board</h1>
+    <p>Create a list of tasks</p>
+    <div class="create-new">
+        <input type="text" 
+        v-model="newTask" 
+        placeholder="Add a new task"
+         @keypress.enter="addTask"
+        />
+        <button @click="addTask">Add</button>
     </div>
+  </main>
 </template>
 
 <script>
