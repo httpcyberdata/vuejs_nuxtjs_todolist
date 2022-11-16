97641337014c7e358ef34e2bed252db94f2a42e0
httpcyberdata <borgeringstad@Brge-Ringstad-PC.local>
2022-11-16 00:10:02 -0500

    delete and undo task functionality works

--- components/Task.vue
+++ components/Task.vue
@@ -4,17 +4,25 @@
        {{ task.content }}
     </div>
     <div class="buttons">
-        <button>
+        <button @click="toggleDone">
            {{ task.done ? 'Undo' : 'Done' }}
         </button>
-        <button class="delete">Delete</button>
+        <button @click="removeTask" class="delete">Delete</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    props: ['task']
+    props: ['task'],
+    methods: {
+        toggleDone() {
+            this.$store.commit('TOGGLE_TASK', this.task);
+        },
+        removeTask() {
+            this.$store.commit('REMOVE_TASK', this.task);
+        }
+    }
 }
 </script>
 
